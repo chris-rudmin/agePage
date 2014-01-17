5d6edf87c8543b1d81db52d5ae3344405a640482
Chris <chris.rudmin@gmail.com>
2014-01-17 05:16:16 -0500

    spaces instead of tabs

--- src/agePage.js
+++ src/agePage.js
@@ -1,71 +1,71 @@
 (function(){
 
-	var displayAge = function( age ) {
-		moment.lang( chrome.i18n.getMessage("@@ui_locale") );
-		$("<div/>")
-			.addClass( "agePageText" )
-			.text( chrome.i18n.getMessage( "published", age.fromNow() ) )
-			.hide()
-			.appendTo( "body" )
-			.fadeIn( 1000 );
-	};
+    var displayAge = function( age ) {
+        moment.lang( chrome.i18n.getMessage("@@ui_locale") );
+        $("<div/>")
+            .addClass( "agePageText" )
+            .text( chrome.i18n.getMessage( "published", age.fromNow() ) )
+            .hide()
+            .appendTo( "body" )
+            .fadeIn( 1000 );
+    };
 
     // Getting last updated date from google failed. Using lastModified date.
     var googleRequestFail = function(){
-    	var lastModifiedDate = moment( document.lastModified );
-    	if ( isOldEnough( lastModifiedDate ) ) {
-    		displayAge( lastModifiedDate );
-    	}
+        var lastModifiedDate = moment( document.lastModified );
+        if ( isOldEnough( lastModifiedDate ) ) {
+            displayAge( lastModifiedDate );
+        }
     };
 
-	// Pages that are always up to date will not show in google search results
-	// NOTE! This is totally bad form, and can break if google changes it's request responses !
-	var googleRequestSuccess = function( data ) {
+    // Pages that are always up to date will not show in google search results
+    // NOTE! This is totally bad form, and can break if google changes it's request responses !
+    var googleRequestSuccess = function( data ) {
 
-		var pageUpdatedDate,
-			urlToSearch = document.URL,
-			titleToSearch = document.title,
-			pageResults = $(data).find("#rso > li");
+        var pageUpdatedDate,
+            urlToSearch = document.URL,
+            titleToSearch = document.title,
+            pageResults = $(data).find("#rso > li");
 
-		pageResults.each( function() {
+        pageResults.each( function() {
 
-			// Long titles and urls have added ellipses. Strip them for matching
-			var $this = $(this),
-				resultUrl = $this.find("cite").text().replace("...", ""),
-				resultTitle = $this.find("h3 a").text().replace(" ...", "");
+            // Long titles and urls have added ellipses. Strip them for matching
+            var $this = $(this),
+                resultUrl = $this.find("cite").text().replace("...", ""),
+                resultTitle = $this.find("h3 a").text().replace(" ...", "");
 
-			// Only use date if url or title match to reduce false positive
-			if ( urlToSearch.indexOf( resultUrl ) != -1 || titleToSearch.indexOf( resultTitle ) != -1 ) {
-				
-				// I've seen dates come back in two different structures. 
-				// Try one then the other
-				pageUpdatedDate = moment( $this.find("span.f").text().split(" - ")[0] );
-				if ( !pageUpdatedDate.isValid() ) {
-					pageUpdatedDate = moment( $this.find("div.f.slp").text().split(" - ")[0] );
-				}
+            // Only use date if url or title match to reduce false positive
+            if ( urlToSearch.indexOf( resultUrl ) != -1 || titleToSearch.indexOf( resultTitle ) != -1 ) {
+                
+                // I've seen dates come back in two different structures. 
+                // Try one then the other
+                pageUpdatedDate = moment( $this.find("span.f").text().split(" - ")[0] );
+                if ( !pageUpdatedDate.isValid() ) {
+                    pageUpdatedDate = moment( $this.find("div.f.slp").text().split(" - ")[0] );
+                }
 
-				return false;
-			}
-		});
+                return false;
+            }
+        });
 
-		( isOldEnough( pageUpdatedDate ) ? displayAge( pageUpdatedDate ) : googleRequestFail() );
+        ( isOldEnough( pageUpdatedDate ) ? displayAge( pageUpdatedDate ) : googleRequestFail() );
     };
 
     var isOldEnough = function( age ){
-    	return ( age && age.isValid() && moment().diff( age, "days" ) > 180 );
+        return ( age && age.isValid() && moment().diff( age, "days" ) > 180 );
     };
 
-	// Search for this URL in google from before 1 day ago
-	var request = $.ajax({
-		url: "https://www.google.com/search",
-		dataType: "html",
-		data : {
-			q: "inurl:"+document.URL, // Search query for this specfic url
-			tbs: "cdr:1,cd_min:,cd_max:", // Custom date rage. This forces the result to show us the page age.
-		}
-	});
+    // Search for this URL in google from before 1 day ago
+    var request = $.ajax({
+        url: "https://www.google.com/search",
+        dataType: "html",
+        data : {
+            q: "inurl:"+document.URL, // Search query for this specfic url
+            tbs: "cdr:1,cd_min:,cd_max:", // Custom date rage. This forces the result to show us the page age.
+        }
+    });
 
-	request.done( googleRequestSuccess );
-	request.fail( googleRequestFail );
+    request.done( googleRequestSuccess );
+    request.fail( googleRequestFail );
 
 }());
